c8889a038e6715cc045fa2b2e2f76612decdd1da
FHtengjiao <fhxtj1991@163.com>
2018-09-14 17:33:38 +0800

    add some data

---
+++ src/com/imooc/utils/JDBCUtil.java
@@ -0,0 +1,48 @@
+package com.imooc.utils;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCUtil {
+
+    private static final ComboPooledDataSource dataSource = new ComboPooledDataSource("mysql");
+
+    public static Connection getConnection() throws SQLException {
+
+        Connection connection = dataSource.getConnection();
+        return connection;
+    }
+
+    public static void release(ResultSet resultSet, Statement statement, Connection connection) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            resultSet = null;
+        }
+
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            statement = null;
+        }
+
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            connection = null;
+        }
+    }
+}
